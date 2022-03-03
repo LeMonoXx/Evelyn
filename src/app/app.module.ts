5a24667b4b7b6941ea42a44b0ef05d7e8802974e
Dustin Gohlke <Dustin.Gohlke@hotmail.de>
2022-03-03 22:40:28 +0100

    Added oauth2 package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { OAuthModule } from 'angular-oauth2-oidc';
+
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+        sendAccessToken: true,
+        allowedUrls: ['http://www.angular.at/api/']
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
+
+export const authConfig: AuthConfig = {
+
+  // Url des Authorization-Servers
+  issuer: 'http://idsvr4.azurewebsites.net',
+
+  // Url der Angular-Anwendung
+  // An diese URL sendet der Authorization-Server den Access Code
+  redirectUri: window.location.origin + '/index.html',
+
+  // Name der Angular-Anwendung
+  clientId: 'spa',
+
+  // Rechte des Benutzers, die die Angular-Anwendung wahrnehmen möchte
+  scope: 'openid profile email offline_access api',
+
+  // Code Flow (PKCE ist standardmäßig bei Nutzung von Code Flow aktiviert)
+  responseType: 'code',
+
+  requireHttps: false
+
+}
+
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +31,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private oauthService: OAuthService) { }
 
   ngOnInit(): void {
+    this.oauthService.configure(authConfig);
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+    this.oauthService.setupAutomaticSilentRefresh();
+    this.oauthService.initLoginFlow();
   }
 
 }
