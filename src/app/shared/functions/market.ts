3e299731a53b218b83d8353730d8faf4b0699e8c
Dustin Gohlke <Dustin.Gohlke@hotmail.de>
2022-05-21 12:45:49 +0200

    fixed bug in price calculation when there are not so many orders

--- src/app/shared/functions/market.ts
+++ src/app/shared/functions/market.ts
@@ -5,33 +5,36 @@ export function getPriceForN(marketEntries: MarketEntry[], amount: number) : Pri
     let amountLeft = amount;
     const result: PriceForN = { totalPrice: 0, averagePrice: 0, enough: false, usedMarketEntries: [] };
 
-  if(marketEntries && marketEntries.length > 0){
+  if(marketEntries && marketEntries.length > 0) {
     // we check our buy-amount against the sell-orders volume_remain and the assigned price.
-    for(let i = 0; i < marketEntries.length - 1; i++) {
+    for(let i = 0; i <= marketEntries.length - 1; i++) {
       const entry = marketEntries[i];
 
-        const useCompleteAmount = (entry.volume_remain - amountLeft) <= 0;
-        if(useCompleteAmount){
-          const costToAdd = (entry.price * entry.volume_remain);
-          
-          totalPrice += costToAdd; 
-          amountLeft -= entry.volume_remain;
-          result.usedMarketEntries.push(entry);
-        } else {
-          const costToAdd = (entry.price * amountLeft);
-          totalPrice += costToAdd;
-          // we used only a portion of this market entry.
-          // so we change the volume_remain with the amount we need to use.
-          entry.volume_remain = amountLeft;
-          result.usedMarketEntries.push(entry);
-          amountLeft = 0;
-        }
-          
-        if(amountLeft <= 0) {
-            result.enough = true;
-            break;
-        }
+      // check if our required amount is more than the current market-entry volumen.
+      // if so, we gonna use the complete market order.
+      const useCompleteOrder = (entry.volume_remain - amountLeft) <= 0;
 
+      if(useCompleteOrder) {
+        const costToAdd = (entry.price * entry.volume_remain);
+        
+        totalPrice += costToAdd; 
+        amountLeft -= entry.volume_remain;
+
+        result.usedMarketEntries.push(entry);
+      } else {
+        const costToAdd = (entry.price * amountLeft);
+        totalPrice += costToAdd;
+        // we used only a portion of this market entry.
+        // so we change the volume_remain with the amount we need to use.
+        // entry.volume_remain -= amountLeft;
+        result.usedMarketEntries.push(entry);
+        amountLeft = 0;
+      }
+
+      if(amountLeft <= 0) {
+          result.enough = true;
+          break;
+      }
     }
 
     result.totalPrice = totalPrice;
